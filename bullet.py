c96dde97667ff25513482e3e21b5e0974ba03375
Tetiana <dattnnsn@gmail.com>
2025-12-27 18:29:00 +0200

    Created bullet

---
+++ bullet.py
@@ -0,0 +1,31 @@
+import pygame
+
+input pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    '''Клас для керування кулями, випущеними з корабля'''
+
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        '''Створити тест кулі у (0, 0) та задати правильну позицію'''
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship_rect.midtop
+
+        '''Зберігати позицію кулі , як десяткове значення'''
+        self.y = float(self.rect.y)
+
+    def update(self):
+        '''Просунути кулю нагору екраном.'''
+        '''Оновити десяткову позицію кулі.'''
+        self.y -= self.settings.bullet_speed
+        '''ОНовити позицію rect'''
+        self.rect.y += self.y
+
+    def draw_bullet(self):
+        '''Намалювати кулю не екрані.'''
+        pygame.draw.rect(self.screen, self.color, self.rect)
