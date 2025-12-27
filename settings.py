40b6e9fd94314b601b6d4f546595ca6be3300507
Tetiana <dattnnsn@gmail.com>
2025-12-27 18:33:35 +0200

    add setiings to bullet

--- settings.py
+++ settings.py
@@ -5,3 +5,8 @@ class Settings(object): #Клас для збереження всіх нала�
         self.bg_color = (230, 230, 230)
         '''Налаштування корабля'''
         self.ship_speed = 1.5
+        '''Налаштування кулі'''
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
