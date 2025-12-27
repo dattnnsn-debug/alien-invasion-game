a098ed7493813498c1bf847c34e83e3f14603cde
Tetiana <dattnnsn@gmail.com>
2025-12-27 16:48:55 +0200

    Added left-right move for ship

--- settings.py
+++ settings.py
@@ -3,3 +3,5 @@ class Settings(object): #Клас для збереження всіх нала�
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        '''Налаштування корабля'''
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship: #Клас для керування корабля
     def __init__(self, ai_game):
         ''' Ініціювати корабель та задати його початкову позицію'''
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         '''Завантажити зображення корабля та отримати його текст'''
@@ -14,6 +15,9 @@ class Ship: #Клас для керування корабля
         '''Створювати кожен новий корабель внизу екрана, по центру.'''
         self.rect.midbottom = self.screen_rect.midbottom
 
+        '''Зберегти десяткове значення позиції корабля по горизонталі'''
+        self.x = float(self.rect.x)
+
         '''Індикатор руху'''
         self.moving_right = False
         self.moving_left = False
@@ -21,9 +25,13 @@ class Ship: #Клас для керування корабля
     def update(self):
         '''Оновити поточну позицію корабля на основі індикатора руху.'''
         if self.moving_right:
-            self.rect.x += 1
+            '''Оновити значення ship.x, а не rect.'''
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        '''Оновити об'єкт rect з self.x.'''
+        self.rect.x = self.x
 
     def blitme(self):
         '''Намалювати корабель у його поточному розташуванні.'''
