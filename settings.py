fcda29bc587c801ec55757fc642df618a3685514
Tetiana <dattnnsn@gmail.com>
2025-12-29 17:40:50 +0200

    Killing aliens

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ class Settings(object): #Клас для збереження всіх нала�
         self.ship_speed = 1.5
         '''Налаштування кулі'''
         self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
