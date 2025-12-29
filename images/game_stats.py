08753af995d4b6ec4f15903e904597b3721fb028
Tetiana <dattnnsn@gmail.com>
2025-12-29 18:06:18 +0200

    Create game_stats.py

---
+++ images/game_stats.py
@@ -0,0 +1,11 @@
+class GameStats:
+    '''Відстежування статистики гри'''
+
+    def __init__(self, ai_game ):
+        '''Ініціалізація статистики'''
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        '''Ініціалізація статистики, що може змінюватися впродовж гри'''
+        self.ships_left = self.settings.ship_limit
